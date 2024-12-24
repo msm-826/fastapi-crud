9e1caf8e8a57bf5d208a7571a742203611bca3df
Muhammed Danish P P <danishmuhammed01@gmail.com>
2024-12-24 23:28:29 +0530

    db connection setup

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from motor import motor_asyncio
+from config import BaseConfig
+
+settings = BaseConfig()
+
+async def lifespan(app: FastAPI):
+    app.client = motor_asyncio.AsyncIOMotorClient(settings.DB_URL)
+    app.db = app.client[settings.DB_NAME]
+
+    try:
+        app.client.admin.command("ping")
+        print("Pinged your deployment. You have successfully connected to MongoDB!")
+        print("Mongo address:", settings.DB_URL)
+    except Exception as e:
+        print(e)
+    
+    yield
+    app.client.close()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def get_root():
+    return {"message": "Root working"}
